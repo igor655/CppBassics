f9fb71f18154ff1b10a6045c7e4e899773dd0b94
Belyy Igor <belyyigor@gmail.com>
2022-11-11 08:33:57 +0200

    ravesli stl sesson 197 listing 1
    create and use stl::vector

--- CppBassics/Source.cpp
+++ CppBassics/Source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<conio.h>
+#include<vector>
 
 using namespace std;
 
@@ -9,6 +10,24 @@ int main()
 {
 
 	cout << "Ravesli stl lessons\n";
+	cout << "vector\n";
+
+	// init vector
+	vector<int> my_vector;
+	for (size_t i = 0; i < 8; i++) 
+	{
+		my_vector.push_back(i + 3);
+	}
+
+	for (size_t i = 0; i < my_vector.size(); i++)
+	{
+		cout << "my_vector: " << i << " = " << my_vector[i] << " ";
+
+	}
+	cout << "\n====================================\n";
+
+
+
 
 	cin.get();
 	return 0;
