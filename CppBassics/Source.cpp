0470601f80f2c04e5ffe76f72f079539ffb1467b
Belyy Igor <belyyigor@gmail.com>
2022-11-11 09:13:04 +0200

    Ravesli 197 list
    not only ravesli
    cplusplus.com

--- CppBassics/Source.cpp
+++ CppBassics/Source.cpp
@@ -2,6 +2,7 @@
 #include<conio.h>
 #include<vector>
 #include<deque>
+#include<list>
 
 
 using namespace std;
@@ -11,48 +12,15 @@ using namespace std;
 int main()
 {
 
-	cout << "Ravesli stl lessons\n";
-	cout << "vector\n";
+	cout << "Ravesli lesson 197 list\n";
 
-	// init vector
-	vector<int> my_vector;
-	for (size_t i = 0; i < 8; i++) 
-	{
-		my_vector.push_back(i + 3);
-	}
-
-	for (size_t i = 0; i < my_vector.size(); i++)
-	{
-		cout << "my_vector: " << i << " = " << my_vector[i] << " ";
-
-	}
-	cout << "\n====================================\n";
-	
-
-	cout << "\n deque\n";
-
-	deque<int> my_deck;
-
-	for (int i = 0; i < 5; ++i)
-	{
-		my_deck.push_back(i);
-		my_deck.push_front(10 - i);
-	}
-
-	//my_deck.assign(4, 55);
-
-	deque<int>::iterator it = my_deck.begin();
-	it += 4;
-	my_deck.insert(it, 55); // insert 55 to index 4;
-
-	// print result
-	for (int i = 0; i < my_deck.size(); ++i)
-	{
-		cout << my_deck[i] << " ";
-	}
-	cout << "\n========================\n";
+	list<int> my_list;
 
 
+
+	my_list.emplace_back(12);
+	my_list.emplace_back(12);
+	my_list.emplace_back(12);
 
 
 	cin.get();
