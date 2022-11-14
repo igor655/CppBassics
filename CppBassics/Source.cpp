8ab91dbcb6d69c9c0bd4b05972948963da9f16a5
Belyy Igor <belyyigor@gmail.com>
2022-11-14 08:27:25 +0200

    Select sort version 2

--- CppBassics/Source.cpp
+++ CppBassics/Source.cpp
@@ -1,100 +1,61 @@
 #include<iostream>
-#include<conio.h>
-#include<vector>
-#include<deque>
-#include<list>
+//#include<conio.h>
 
 
 using namespace std;
+template<class T>
+void print(T*, int n);
+
+template<class T> 
+void selectSort(T*, int n);
 
 
 
 int main()
 {
 
-	cout << "Ravesli lesson 197 list\n";
+	cout << "Select sort\n";
 
-	list<int> my_list;
+	int arr[]{ 12, 19, 4, 8, 9, 2,5, -3, -4, 11 };
+	int N = sizeof(arr) / sizeof(int);
+	cout << "arr before sort\n";
+	print(arr, N);
+	selectSort(arr, N);
+	cout << "arrr after select sort\n";
+	print(arr, N);
 
-
-
-	my_list.emplace_back(12);
-	my_list.emplace_back(1);
-	my_list.emplace_back(6);
-	my_list.emplace_front(10);
-	my_list.emplace_front(22);
-
-
-
-	//print
-	cout << "The contents of my_list are: \n";
-	for (list<int>::iterator it = my_list.begin(); it != my_list.end(); ++it)
-	{
-		cout << *it << " ";
-	}
-	cout << "\n=========================\n";
-
-	list<int> first_list(4, 100);
-	cout << first_list.size() << endl;
-	cout << "The contents of first_list are: \n";
-
-	for (auto it = first_list.begin(); it != first_list.end(); ++it)
-	{
-		cout << *it << " ";
-	}
-	cout << "\n=========================\n";
-
-	cout << "constructor init second_list with first_list\n";
-	list<int> second_list(first_list.begin(), first_list.end());
-
-	cout << "The contents of second_list are: \n";
-	for (auto it = second_list.begin(); it != second_list.end(); it++)
-	{
-		cout << *it << "\t";
-	}
-	cout << "\n=========================\n";
-
-	cout << "copy second_list ti third_list " << endl;
-	list<int> third_list(second_list);
-	// add elements
-	cout << "add two elements\n";
-	third_list.push_back(12);
-	third_list.push_front(11);
-
-	// 
-	cout << "Contents of third_list are: \n";
-
-	for (list<int>::iterator it2 = third_list.begin(); it2 != third_list.end(); ++it2)
-	{
-		cout << *it2 << " ";
-	}
-	cout << "\n=========================\n";
-
-
-	cout << "The iterator constructor can also be used to construct from array\n";
-
-	int arr[]{ 3, 6, 1, 44 };
-	list<int> fifth_list(arr, arr + sizeof(arr) / sizeof(int));
-
-
-
-	cout << "The contents of fifth_list are: \n";
-	for (list<int>::iterator it = fifth_list.begin(); it != fifth_list.end(); it++)
-	{
-		cout << *it << "\t";
-	}
-	cout << "\n=========================\n";
-	cout << "sort fifth_list\n";
-	fifth_list.sort();
-	cout << "Contents of fifth_list after sort are: \n";
-	for (list<int>::iterator it = fifth_list.begin(); it != fifth_list.end(); it++)
-	{
-		cout << *it << "\t";
-	}
-
-
-	fifth_list.sort();
-
+	
 	cin.get();
 	return 0;
+}
+
+template<class T>
+void print(T* arr, int n)
+{
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n---------------------------------\n";
+}
+
+
+template<class T>
+void selectSort(T* arr, int n)
+{
+	for (size_t i = 0; i < n-1; i++)
+	{ 
+		int index = i; // index of min element
+		for (size_t j = i+1; j < n; j++)
+		{
+			if (arr[j] < arr[index])
+			{
+				index = j;
+			}
+
+		}
+		//swap
+		swap(arr[i], arr[index]);
+
+	}
 }
