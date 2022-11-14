822b8544ed69055c5f8a975dfc4a7ad2ff76bba9
Belyy Igor <belyyigor@gmail.com>
2022-11-14 08:39:33 +0200

    Clean code
    merge sort

--- CppBassics/Source.cpp
+++ CppBassics/Source.cpp
@@ -6,8 +6,6 @@ using namespace std;
 template<class T>
 void print(T*, int n);
 
-template<class T> 
-void selectSort(T*, int n);
 
 
 
@@ -16,15 +14,8 @@ int main()
 
 	cout << "Select sort\n";
 
-	int arr[]{ 12, 19, 4, 8, 9, 2,5, -3, -4, 11, -22 };
-	int N = sizeof(arr) / sizeof(int);
-	cout << "arr before sort\n";
-	print(arr, N);
-	selectSort(arr, N);
-	cout << "arrr after select sort\n";
-	print(arr, N);
-
 	
+
 	cin.get();
 	return 0;
 }
@@ -40,26 +31,4 @@ void print(T* arr, int n)
 }
 
 
-template<class T>
-void selectSort(T* arr, int n)
-{
-	for (size_t i = 0; i < n-1; i++)
-	{ 
-		int index = i; // index of min element
-		for (size_t j = i+1; j < n; j++)
-		{
-			if (arr[j] < arr[index])
-			{
-				index = j;
-			}
 
-		}
-		//swap
-		//swap(arr[i], arr[index]);
-
-		T tmp = arr[i];
-		arr[i] = arr[index];
-		arr[index] = tmp;
-
-	}
-}
